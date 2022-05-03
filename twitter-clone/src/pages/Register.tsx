1bebf59882ec77c03c6b46261bbc0ca3a6417022
lucas <lucmaieski@gmail.com>
2022-05-03 04:46:42 -0300

    Set initial Register CSS

--- twitter-clone/src/pages/Register.tsx
+++ twitter-clone/src/pages/Register.tsx
@@ -2,9 +2,9 @@ import { RegisterField } from '../components/RegisterField';
 
 export function Register() {
   return (
-    <div>
+    <div className='welcome-page'>
       <div className="register-form">
-        <h2 className="form-name">Registrar-se</h2>
+        <h2 className="register-name">Registrar-se</h2>
         <RegisterField />
       </div>
     </div>
