372b8876a7d3bf5b1956373e0ea594772a28714a
lucas <lucmaieski@gmail.com>
2022-05-23 21:07:49 -0300

    Implements home btns functions

--- twitter-clone/src/components/Header.tsx
+++ twitter-clone/src/components/Header.tsx
@@ -2,10 +2,16 @@ import { BiHomeAlt } from 'react-icons/bi';
 import { BiExit } from "react-icons/bi";
 
 export function Header() {
+  const redirectHomePage = () => {
+    window.scrollTo({
+      top: 0
+    })
+  }
+  
   return (
     <div>
       <header className="header">
-        <button className="home-btn-header"><BiHomeAlt size="2em" /></button>
+        <button onClick={redirectHomePage} className="home-btn-header"><BiHomeAlt size="2em" /></button>
         <h1 className="main-page-txt">Main page</h1>
         <button className="exit-btn-header"><BiExit size="2em" /></button>
       </header>
--- twitter-clone/src/components/OptionsBar.tsx
+++ twitter-clone/src/components/OptionsBar.tsx
@@ -1,9 +1,15 @@
 import { BiHomeAlt, BiSearchAlt, BiUser } from 'react-icons/bi';
 
 export function OptionsBar() {
+  const redirectHomePage = () => {
+    window.scrollTo({
+      top: 0
+    })
+  }
+  
   return (
     <div className="option-body-div">
-      <button className="option-btn"><BiHomeAlt size="2em" /></button>
+      <button onClick={redirectHomePage} className="option-btn"><BiHomeAlt size="2em" /></button>
       <button className="option-btn"><BiSearchAlt size="2em" /></button>
       <button className="option-btn"><BiUser size="2em" /></button>
     </div>
