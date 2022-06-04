f0dc4b5cde950c98badbbe4b94886dd4103994a0
lucas <lucmaieski@gmail.com>
2022-06-04 18:32:10 -0300

    Set ivalidUserPage css

--- twitter-clone/src/pages/InvalidUser.tsx
+++ twitter-clone/src/pages/InvalidUser.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import '../css/InvalidUserPage.css';
 
 export default function InvalidUser() {
   return (
-    <div>
-      <h2>Você não tem permisão para acessar esta pagina!</h2>
-      <span>Talvez o seu token de acesso tenha expirado, faça o login novamente!</span>
-      <span>volte para a pagina inicial clicando </span>
-      <Link to="/">AQUI</Link>
+    <div className='invalidUser-container'>
+      <h2 className='invalidUser-title'>Você não tem permisão para acessar esta pagina!</h2>
+      <div className='invalidUser-text-container'>
+        <span>Talvez o seu token de acesso tenha expirado, faça o login novamente!</span>
+        <span>volte para a pagina inicial clicando </span>
+      </div>
+      <Link className='invalidUser-link' to="/">AQUI</Link>
     </div>
   );
 }
